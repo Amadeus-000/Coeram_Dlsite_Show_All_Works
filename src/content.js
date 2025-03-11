db426f2344cc6d285d45c1a609a708964b8c8bb1
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2025-03-12 03:19:39 +0900

    first commit

---
+++ src/content.js
@@ -0,0 +1,26 @@
+(function () {
+   console.log("DLsite Auto Not Purchased Filter が実行されました");
+ 
+   function clickNotPurchasedFilter() {
+     // PC版とモバイル版の `a` タグを取得
+     const filterLinks = document.querySelectorAll("#hide_bought a, a#hide_bought");
+ 
+     if (filterLinks.length > 0) {
+       console.log(`見つかったボタン数: ${filterLinks.length}`);
+       filterLinks.forEach((link) => {
+         console.log("フィルタをクリックします:", link);
+         link.click();
+       });
+       return true;
+     } else {
+       console.log("ボタンが見つかりません");
+       return false;
+     }
+   }
+ 
+   if(!clickNotPurchasedFilter()){
+      console.log("3秒後に再度実行します");
+      setTimeout(clickNotPurchasedFilter, 3000);
+   }
+ })();
+ 
